809dae5f626c0c1ff7ce7dc7511abd0e7e577fc6
chianw <chiancw@gmail.com>
2025-03-08 14:06:38 +0800

    set use_cli to false in azurerm provider

--- providers.tf
+++ providers.tf
@@ -11,15 +11,16 @@ terraform {
       version = "4.14.0"
     }
     random = {
-        source = "hashicorp/random"
-        version = "3.7.1"
-    }    
+      source  = "hashicorp/random"
+      version = "3.7.1"
+    }
   }
 }
 
 provider "azurerm" {
   features {}
-
+  use_cli         = false
+  tenant_id       = "333d80f4-dd33-4824-8d53-cefe747d7677"
   subscription_id = "68be2809-9674-447c-a43d-261ef2862c29"
 }
 
